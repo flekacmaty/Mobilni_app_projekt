344c8b4cb61acc8c93c3625d7258ded6d88814b8
flekacmaty <flekac.matyas@seznam.cz>
2025-01-02 14:24:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Flekac_projekt',
+  webDir: 'www'
+};
+
+export default config;
