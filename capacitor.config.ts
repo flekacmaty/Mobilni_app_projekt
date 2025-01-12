ddfc10cd708e54dc5c43ad2a2806fa03ee9d12ae
flekacmaty <flekac.matyas@seznam.cz>
2025-01-12 13:35:28 +0100

    Přídání android aplikace, vytvoření splashscreen a ikony

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Flekac_projekt',
-  webDir: 'www'
-};
+  webDir: 'www',
+
+plugins: {
+  SplashScreen: {
+    launchShowDuration: 3000,
+    launchAutoHide: true,
+    backgroundColor: '#ffffffff',
+    androidSplashResourceName: 'splash',
+    iosSplashResourceName: 'Default',
+    showSpinner: true, 
+    spinnerColor: '#000000', 
+  },
+},
+}
 
 export default config;
